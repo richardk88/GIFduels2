3bf71b7274d0b0ae7962585ed88f04e1fa4969cf
danremel <remel.dan@gmail.com>
2017-07-30 18:59:42 -0400

    working on user routes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost/GIFduels2');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
-const gifs = require('./routes/gifs')
+// const gifs = require('./routes/gifs')
 
 
 const app = express();
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/users/:id/gifs', gifs);
+// app.use('/users/:id/gifs', gifs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,125 +1,147 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
+const Battle = require('../models/user');
 const User = require('../models/user');
 
+
 // index router
-router.get('/', (req, res) => {
-  User.find({}).then((users) => {
+router.get('/', (req, res) =>{
+  Battle.find({}).then ((battles) =>{
+    console.log(battles);
+    // res.send(battles[0])
     res.render(
-      'users/index', {
-        users
-      }
-    );
-  }).catch((error) => {
-    console.log('Error retrieving users from database!');
-    console.log(error);
-  });
-});
+        'users/index',
+      {
 
-//create user
-router.get('/new', (req, res) => {
-  res.render('users/new');
-});
+        users: battles,
+        userId: battles._id
 
-router.post('/', (req, res) => {
-  const newUserForm = req.body;
-  User.create(newUserForm)
-    .then((user) => {
-      res.render(
-        'users/show', {
-        userId: user._id,
-        userName: user.userName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email
-        // gif: user.gifs[0].imgUrl
-        });
-    }).catch((error) => {
-      console.log('Error saving new user to database!');
-      console.log(error);
-    })
-})
-
-// Show route
-router.get('/:id', (req, res) => {
-  const userId = req.params.id;
-  User.findById(userId).then((user) => {
-      console.log(user);
-      res.render('users/show', {
-        userId: user._id,
-        userName: user.userName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email
-        // gif: user.gifs[0].imgUrl
-      })
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-
-
-
-
-//Delete Route
-router.get('/:id/delete', (req, res) => {
-  const userIdToDelete = req.params.id;
-
-  User.findByIdAndRemove(userIdToDelete).then(() => {
-    console.log('HOORAY');
-    res.redirect('/users')
-  });
-});
-
-//Render Edit Form For User
-router.get('/:id/edit', (req, res) => {
-  const userIdToFind = req.params.id;
-  User.findById(userIdToFind)
-    .then( (user) => {
-      res.render('../views/users/edit', {
-        userId: user._id,
-        userName: user.userName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email
-        // gif: user.gifs[0].imgUrl
       }
       );
-    })
-})
-//Update Route
-router.put('/:id', (req, res) => {
-
-  const userIdToUpdate = req.params.id;
-  const updatedUserInfo = req.body;
-
-  User.findByIdAndUpdate(
-      userIdToUpdate,
-      updatedUserInfo,
-      {new: true} 
-  ).then((user) => {
-    console.log(`User with ID of ${user._id} updated!`);
-
-    res.render(
-        'users/show',
-          {
-        userId: user._id,
-        userName: user.userName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email
-        // gif: user.gifs[0].imgUrl
-      }
-    );
-  }).catch((error) => {
-    console.log(`User with ID of ${user._id} failed to update!`);
+  }).catch((error) =>{
     console.log(error);
-  });
-
+   });
 });
+
+// // index router
+
+// router.get('/', (req, res) => {
+//   User.find({}).then((users) => {
+//     res.render(
+//       'users/index', {
+//         users
+//       }
+//     );
+//   }).catch((error) => {
+//     console.log('Error retrieving users from database!');
+//     console.log(error);
+//   });
+// });
+
+// //create user
+// router.get('/new', (req, res) => {
+//   res.render('users/new');
+// });
+
+// router.post('/', (req, res) => {
+//   const newUserForm = req.body;
+//   User.create(newUserForm)
+//     .then((user) => {
+//       res.render(
+//         'users/show', {
+//         userId: user._id,
+//         userName: user.userName,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email
+//         // gif: user.gifs[0].imgUrl
+//         });
+//     }).catch((error) => {
+//       console.log('Error saving new user to database!');
+//       console.log(error);
+//     })
+// })
+
+// // Show route
+// router.get('/:id', (req, res) => {
+//   const userId = req.params.id;
+//   User.findById(userId).then((user) => {
+//       console.log(user);
+//       res.render('users/show', {
+//         userId: user._id,
+//         userName: user.userName,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email
+//         // gif: user.gifs[0].imgUrl
+//       })
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
+
+
+
+
+// //Delete Route
+// router.get('/:id/delete', (req, res) => {
+//   const userIdToDelete = req.params.id;
+
+//   User.findByIdAndRemove(userIdToDelete).then(() => {
+//     console.log('HOORAY');
+//     res.redirect('/users')
+//   });
+// });
+
+// //Render Edit Form For User
+// router.get('/:id/edit', (req, res) => {
+//   const userIdToFind = req.params.id;
+//   User.findById(userIdToFind)
+//     .then( (user) => {
+//       res.render('../views/users/edit', {
+//         userId: user._id,
+//         userName: user.userName,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email
+//         // gif: user.gifs[0].imgUrl
+//       }
+//       );
+//     })
+// })
+// //Update Route
+// router.put('/:id', (req, res) => {
+
+//   const userIdToUpdate = req.params.id;
+//   const updatedUserInfo = req.body;
+
+//   User.findByIdAndUpdate(
+//       userIdToUpdate,
+//       updatedUserInfo,
+//       {new: true} 
+//   ).then((user) => {
+//     console.log(`User with ID of ${user._id} updated!`);
+
+//     res.render(
+//         'users/show',
+//           {
+//         userId: user._id,
+//         userName: user.userName,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email
+//         // gif: user.gifs[0].imgUrl
+//       }
+//     );
+//   }).catch((error) => {
+//     console.log(`User with ID of ${user._id} failed to update!`);
+//     console.log(error);
+//   });
+
+// });
 
 
 module.exports = router;
