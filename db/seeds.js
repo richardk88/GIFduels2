7f3fac037a8c09bb14e708cc6cfeb388a922ce5e
djskim27 <djs.kim27@gmail.com>
2017-07-31 10:00:27 -0400

    trying heroku

--- db/seeds.js
+++ db/seeds.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/GIFduels2');
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost/GIFduels2');
+mongoose.connect(process.env.MONGODB_URI);
 
 const Battle = require('../models/battle');
 const User = require('../models/user');
