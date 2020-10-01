c84824c18e6a95321728fc36d62b239e93335f1e
Richard Kim <richardk88@hotmail.com>
2020-10-01 11:34:12 -0400

    updating to ternary for cleaner code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,13 +3,13 @@ $(function() {
 //click event for gif1 win
 $('.animated-gif').on('click', function(val){
 
-    if (val.target.className.indexOf('gif1') > -1) {
+    (val.target.className.indexOf('gif1') > -1) ? (
         $('.gif1').css('border','20px solid green');
         $('.gif2').off();
-    } else {
+    ) : (
         $('.gif2').css('border','20px solid green');
         $('.gif1').off();
-    }
+    )}
     function newDuel() {
         window.location.reload();
     }
